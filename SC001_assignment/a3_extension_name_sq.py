9f834d96caa927016ae8a8fa5e174b240520ee07
Sanny Lin <150927857+sannylin325@users.noreply.github.com>
2024-01-24 14:41:52 +0800

    Add files via upload

---
+++ SC001_assignment/a3_extension_name_sq.py
@@ -0,0 +1,45 @@
+"""
+File: name_sq.py (extension)
+Name: Sanny Lin
+----------------------------
+(Instructions by stanCode)
+This program is an extension of assignment3!
+It will ask the user to provide a name, 
+and the square pattern of the given name 
+will be printed on the console.
+"""
+
+
+def main():
+    """
+    function: make a square pattern, which is circled by the inputted name
+    """
+    print("This program prints a name in a square pattern!")
+    name = input("Name: ")
+    my_name_pattern(name)
+
+
+def my_name_pattern(name):
+    """
+    function: district the pattern into 3 parts, print in sequence
+    (1) name
+    (2) vertical words
+    (3) name in reverse order
+    """
+    # part(1): name
+    print(str(name))
+    # part(2): vertical words
+    for i in range(1, len(name)-1):
+        print(name[i], end="")
+        for j in range(len(name)-2):
+            print(" ", end="")
+        print(name[len(name)-i-1])
+    # part(3): name in reverse order
+    for i in range(len(name)):
+        print(name[len(name)-i-1], end="")
+
+
+# DO NOT EDIT CODE BELOW THIS LINE #
+
+if __name__ == '__main__':
+    main()
---
+++ SC001_assignment/a3_extension_similarity.py
@@ -0,0 +1,66 @@
+"""
+File: similarity.py (extension)
+Name: Sanny Lin
+----------------------------
+(Instructions by stanCode)
+This program is an extension of assignment3!
+It will compare short dna sequence, s2,
+with sub sequences of a long dna sequence, s1
+The way of approaching this task is the same as
+what people are doing in the bio industry.
+"""
+
+
+def main():
+    """
+    program: compare the short_sequence with the long_sequence,
+             find the most match piece in long_sequence
+    """
+    ans = match()
+    print("The best match is " + ans)
+
+
+def match():
+    """
+    function:
+    1. cut the long_sequence into short_digit sizes at the beginning of index[i]
+       ("i" is a loop from 0 to (long_digit-short_digit+1))
+    2. compare short_sequence with each cut_sequences
+        (1) if short_sequence[j] == cut_sequence[j]: get 1 score
+        (2) if score > high_score:
+            reassign high_score
+            reassign ans = cut_sequence
+    :return ans: str, which is the most match cut_sequence from long_sequence
+    """
+    # input DNA sequences (case-insensitive)
+    long_sequence = input("Please give me a DNA sequence to search: ").upper()
+    short_sequence = input("What DNA sequence would you like to match? ").upper()
+
+    # digits of the DNA sequences
+    long_digit = len(long_sequence)
+    short_digit = len(short_sequence)
+
+    # set variables
+    score, high_score = 0, 0
+    ans = ""
+
+    # cut long_sequence into short_digit sizes at the beginning of index[i]
+    # the above "i" is a loop from 0 to (long_digit-short_digit+1)
+    for i in range(0, long_digit - short_digit + 1):
+        cut_sequence = long_sequence[i:(i + short_digit)]
+        score = 0  # clear the old score value before comparing new cut_sequence
+        # compare short_sequence with each cut_sequences
+        for j in range(short_digit):
+            if short_sequence[j] == cut_sequence[j]:
+                score += 1
+        if score > high_score:
+            high_score = score
+            ans = cut_sequence
+
+    return ans
+
+
+# DO NOT EDIT CODE BELOW THIS LINE #
+
+if __name__ == '__main__':
+    main()
