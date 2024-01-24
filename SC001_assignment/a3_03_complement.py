7a79db7993fe3f4e8693795d4fd220293b0ee350
Sanny Lin <150927857+sannylin325@users.noreply.github.com>
2024-01-24 12:13:12 +0800

    Add files via upload

---
+++ SC001_assignment/a3_03_complement.py
@@ -0,0 +1,51 @@
+"""
+File: complement.py
+Name: Sanny Lin
+----------------------------
+(Instructions by stanCode)
+This program uses string manipulation to
+tackle a real world problem - finding the
+complement strand of a DNA sequence.
+THe program provides different DNA sequence as
+a python string that is case-sensitive.
+Your job is to output the complement of them.
+"""
+
+
+def main():
+    """
+    function: reply the complement strand of the inputted DNA sequence
+    """
+    print(build_complement('ATC'))
+    print(build_complement(''))
+    print(build_complement('ATGCAT'))
+    print(build_complement('GCTATAC'))
+
+
+def build_complement(dna):
+    """
+    :param dna: str, inputted DNA sequence
+    :return result: str, which is the string of the complement strand of the inputted DNA sequence
+                    regulation: "A" <-> "T" ; "C" <-> "G"
+    * if no inputted DNA sequence: print "DNA strand is missing."
+    """
+    result = ''
+    if dna == '':
+        print('DNA strand is missing.', end="")  # clean the next empty line
+    else:
+        for i in range(len(dna)):
+            if dna[i] == 'A':
+                result += 'T'
+            elif dna[i] == 'T':
+                result += 'A'
+            elif dna[i] == 'C':
+                result += 'G'
+            elif dna[i] == 'G':
+                result += 'C'
+    return result
+
+
+# DO NOT EDIT CODE BELOW THIS LINE #
+
+if __name__ == '__main__':
+    main()
